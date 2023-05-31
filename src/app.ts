4cf01e1840e30e68f1ec455c4d90d94893539d34
joao.silveira@estudante.ufla.br <joao.silveira@estudante.ufla.br>
2023-05-31 16:44:41 -0300

    feat: initial setup

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import { env } from './env'
+import { app } from './server'
+
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(`Server Http is running in port ${env.PORT}`)
+  })
