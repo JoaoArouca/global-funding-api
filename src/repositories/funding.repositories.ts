a6958d4f4f926c0a5a26456cd41ea40f71119f30
joao.silveira@estudante.ufla.br <joao.silveira@estudante.ufla.br>
2023-06-16 14:18:16 -0300

    feat: update method funding repository

--- src/repositories/funding.repositories.ts
+++ src/repositories/funding.repositories.ts
@@ -3,4 +3,5 @@ import { Prisma, funding } from '@prisma/client'
 export interface FundingRepository {
   findByKey(hash: string): Promise<funding | null>
   create(data: Prisma.fundingCreateInput): Promise<funding>
+  getById(id: string): Promise<funding | null>
 }
