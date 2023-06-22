9a17ec1d1c9beb516e5c42270ba2d235b3c6e786
JoaoArouca <joao.silveira@estudante.ufla.br>
2023-06-22 13:42:08 -0300

    feat: change auth hash to bcryptjs

--- src/http/middleware/verify-auth-hash.ts
+++ src/http/middleware/verify-auth-hash.ts
@@ -1,10 +1,10 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
-import { Decrypt } from '../../utils/decrypt'
-import { env } from '../../env'
 import { MakeGetPreUsersUseCase } from '../../services/factories/make-get-preusers.use-case'
 import { AuthorizationTokenNotFoundError } from '../../services/errors/authorization-token-not-found.error'
 import { NonStandartTokenRecievedError } from '../../services/errors/non-standart-token-recieved.error'
 import { UnauthorizedUserError } from '../../services/errors/unauthorized-user.error'
+import { compareSync } from 'bcryptjs'
+import { prisma } from '../../lib/prisma'
 
 export async function verifyHash(request: FastifyRequest, reply: FastifyReply) {
   try {
@@ -14,17 +14,19 @@ export async function verifyHash(request: FastifyRequest, reply: FastifyReply) {
       throw new AuthorizationTokenNotFoundError()
     }
 
-    const decodeHash = Decrypt(token as string, env.HASH_KEY)
+    const users = await prisma.preUser.findMany() // TODO get all users use case
 
-    if (!decodeHash) {
-      throw new NonStandartTokenRecievedError()
+    const matchedUsers = users.filter((user) =>
+      compareSync(user.email, token as string),
+    )
+
+    if (matchedUsers.length > 0) {
+      const getPreUserByEmail = MakeGetPreUsersUseCase()
+      const user = await getPreUserByEmail.execute(matchedUsers[0].email)
+      request.preUser = user.user
+    } else {
+      throw new UnauthorizedUserError()
     }
-
-    const getPreUserByEmail = MakeGetPreUsersUseCase()
-
-    const user = await getPreUserByEmail.execute(decodeHash)
-
-    request.preUser = user.user
   } catch (error) {
     if (error instanceof AuthorizationTokenNotFoundError) {
       return reply.status(409).send({ message: error.message })
